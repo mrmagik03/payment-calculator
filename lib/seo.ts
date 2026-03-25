d3822be505f559bb8ac7153f43bf1af26cda4044
mrmagik03 <targetdillo@gmail.com>
2026-03-25 16:40:53 -0500

    Update SITE_URL to new domain

--- lib/seo.ts
+++ lib/seo.ts
@@ -1,1 +1,1 @@
-export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://payment-calculator.co";
+export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://monthlypaymentcalc.co";
